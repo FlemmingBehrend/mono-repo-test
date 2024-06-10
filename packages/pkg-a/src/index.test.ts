c107f7b28e8ed9ad54927a3afa75e176651e5080
Flemming Behrend <topfeb@gmail.com>
2024-06-10 16:22:45 +0200

    up

--- packages/pkg-a/src/index.test.ts
+++ packages/pkg-a/src/index.test.ts
@@ -1,6 +1,6 @@
 import { helloFromPkgA } from './index';
 describe('pkg-a', () => {
   it('should work', () => {
-    expect(helloFromPkgA()).toBe("Hello from pkg-a package 2");
+    expect(helloFromPkgA()).toBe("Hello from pkg-a package 10");
   });
 });
